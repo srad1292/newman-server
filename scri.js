0e1d0efde8c7c946549c932d0707635e1e159029
Sam Radford <smr1292@yahoo.com>
2020-01-23 22:14:27 -0500

    script example

--- scri.js
+++ scri.js
@@ -1,23 +1,62 @@
 
 
+
+require('dotenv').config();
+
+const { 
+    DATABASE,
+    SCHEMA,
+    DB_USERNAME,
+    PORT,
+    PASSWORD
+} = process.env;
+
+console.log({ 
+    DATABASE,
+    SCHEMA,
+    DB_USERNAME,
+    PORT,
+    PASSWORD
+} );
+
 let knex = require('knex')({
     client: 'pg',
-    searchPath: ['public', 'newman'],
+    searchPath: ['public', SCHEMA],
     connection: {
       host : 'localhost',
-      port : '5432',
-      user : 'postgres',
-      password : 'mcbrother1292',
-      database : 'postgres'
+      port : PORT,
+      user : DB_USERNAME,
+      password : PASSWORD,
+      database : DATABASE
     }
 });
 
-getDoctors();
-
+// let knex2 = require('knex')({
+//     client: 'pg',
+//     searchPath: ['public', 'newman'],
+//     connection: {
+//       host : 'localhost',
+//       port : '5432',
+//       user : 'postgres',
+//       password : 'mcbrother1292',
+//       database : 'postgres'
+//     }
+// });
 
 // const db = require('knex')(knex(configOptions));
+
+
+async function main() {
+    await getDoctors();
+    // await createTables();
+    // await fdciMigrations();
+    process.exit(); 
+}
+
 
 async function getDoctors() {
     const doctors = await knex.select("*").from("doctor");
     console.log(doctors);
 }
+
+main(); 
